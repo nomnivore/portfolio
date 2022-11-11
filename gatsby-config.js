96d39a282028213a0a2acbabff8ac755df040098
Nomnivore <6979410+Nomnivore@users.noreply.github.com>
2022-11-11 16:40:31 -0500

    blank site

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,10 +9,9 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    title: `KW`,
+    description: `Kyle's personal portfolio as a web developer. Built using Gatsby`,
+    author: `nomnivore`,
   },
   plugins: [
     `gatsby-plugin-image`,
