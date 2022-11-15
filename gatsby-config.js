4387a2ede9d3c82b021be62035dcf2a51f30b54e
Nomnivore <6979410+Nomnivore@users.noreply.github.com>
2022-11-15 01:15:42 -0500

    update manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,15 +27,15 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `KW`,
+        short_name: `KW`,
         start_url: `/`,
-        background_color: `#663399`,
+        background_color: `#111827`,
         // This will impact how browsers show your PWA/website
         // https://css-tricks.com/meta-theme-color-and-trickery/
         // theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/favicon.svg`, // This path is relative to the root of the site.
       },
     },
     {
