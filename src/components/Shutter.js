a8992ced8a0087ea6167e95479288c9ac406a671
Nomnivore <6979410+Nomnivore@users.noreply.github.com>
2022-11-15 21:48:17 -0500

    use framer motion for shutter

--- src/components/Shutter.js
+++ src/components/Shutter.js
@@ -1,27 +1,36 @@
 import { StaticImage } from "gatsby-plugin-image"
 import React, { useRef } from "react"
 import useToggle from "../hooks/useToggle"
-import "../styles/Shutter.css"
+// import "../styles/Shutter.css"
+import { motion } from "framer-motion"
 
 // TODO: integrate framer-motion here
 export const Shutter = () => {
   const shutterRef = useRef(null)
   const [visible, toggleVisible] = useToggle(true)
   return visible ? (
-    <div
+    <motion.div
       ref={shutterRef}
-      onAnimationEnd={ev =>
-        ev.target === shutterRef.current && toggleVisible(false)
-      }
-      className="shutter absolute top-0 left-0 w-full h-full bg-neutral-900 z-50 flex justify-center items-center shadow-2xl"
+      className="absolute top-0 left-0 w-full h-full bg-neutral-900 z-50 flex justify-center items-center shadow-2xl"
+      initial={{ y: 0 }}
+      animate={{ y: "-100%" }}
+      transition={{ delay: 2, duration: 2, ease: [0.39, 0.1, 0.25, 0.9] }}
+      onAnimationComplete={() => toggleVisible(false)}
     >
-      <StaticImage
-        src="../images/logo.png"
-        className="shutter-logo max-w-2xl mx-3"
-        alt="KW logo"
-        placeholder="none"
-      />
-    </div>
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ ease: "linear", duration: 1, delay: 0.2 }}
+        className="flex justify-center items-center"
+      >
+        <StaticImage
+          src="../images/logo.png"
+          className="max-w-2xl mx-3"
+          alt="KW logo"
+          placeholder="none"
+        />
+      </motion.div>
+    </motion.div>
   ) : (
     <></>
   )
